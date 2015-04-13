9df608b7bd707c2006e4d75ad30e418cb4588bea
sonbua <dinh.son.nguyen@gmail.com>
2015-04-13 09:26:29 +0700

    Check argument for null.

--- src/Oryza.Utility/DocumentSessionExtensions.cs
+++ src/Oryza.Utility/DocumentSessionExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Oryza.Entities;
 using Raven.Client;
 
@@ -8,6 +9,16 @@ namespace Oryza.Utility
     {
         public static void BatchStore<T>(this IDocumentSession session, IEnumerable<T> entities) where T : IEntity
         {
+            if (session == null)
+            {
+                throw new ArgumentNullException("session");
+            }
+
+            if (entities == null)
+            {
+                throw new ArgumentNullException("entities");
+            }
+
             foreach (var entity in entities)
             {
                 session.Store(entity);
