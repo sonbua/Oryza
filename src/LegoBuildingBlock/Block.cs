c21ca18456657b83c3f5c84c885221913a1901f5
sonbua <dinh.son.nguyen@gmail.com>
2015-07-29 09:45:04 +0700

    Add basic Block implementation.

---
+++ src/LegoBuildingBlock/Block.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LegoBuildingBlock
+{
+    public class Block<TInput, TOutput> : IBlock<TInput, TOutput>
+    {
+        public Block(Func<TInput, TOutput> handle)
+        {
+            if (handle == null)
+            {
+                throw new ArgumentNullException("handle");
+            }
+
+            Handle = handle;
+        }
+
+        public Func<TInput, TOutput> Handle { get; private set; }
+    }
+}
