398e13f347d9973cca997aa847d3ed5287918c62
sonbua <dinh.son.nguyen@gmail.com>
2015-07-31 08:58:47 +0700

    Add IfElseBlock.

---
+++ src/LegoBuildingBlock/IfElseBlock.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LegoBuildingBlock
+{
+    public class IfElseBlock<TInput, TOutput> : IBlock<TInput, TOutput>
+    {
+        public IfElseBlock(Func<bool> testFunc, IBlock<TInput, TOutput> trueBlock, IBlock<TInput, TOutput> falseBlock)
+        {
+            if (testFunc())
+            {
+                Handle = input => trueBlock.Handle(input);
+            }
+            else
+            {
+                Handle = input => falseBlock.Handle(input);
+            }
+        }
+
+        public Func<TInput, TOutput> Handle { get; private set; }
+    }
+}
